cc799468736833ebfb1d36a63e0533aaa5e15d18
Alexander Entinger <cto@lxrobotics.com>
2019-10-28 16:33:20 +0100

    Perform preinit/init/fini execution in startup code

--- hal/FE310/hal-fe310-digital-out-pin/startup.cpp
+++ hal/FE310/hal-fe310-digital-out-pin/startup.cpp
@@ -6,6 +6,9 @@ extern "C" void __start(void) __attribute__ ((noreturn));
 
 void init_bss();
 void init_data();
+void preinit_array();
+void init_array();
+void fini_array();
 
 extern "C" void __start(void)
 {
@@ -16,8 +19,15 @@ extern "C" void __start(void)
   /* Perform the ROM-to-RAM initialisation */
   init_data();
 
+  /* Perform preinit/init steps - call static ctors */
+  preinit_array();
+  init_array();
+
   /* Jump to main */
   asm volatile("call main");
+
+  /* In case we should return from main perform deinitialisation - calling static dtors */
+  fini_array();
 
   for(;;)
   {
@@ -46,4 +56,49 @@ void init_data()
   std::copy(&__data_src_start,
             &__data_src_start + cnt,
             &__data_dst_start);
+}
+
+typedef void(*PreInitFuncType)(void);
+
+extern PreInitFuncType __preinit_array_start[];
+extern PreInitFuncType __preinit_array_end[];
+
+void preinit_array()
+{
+  std::for_each(__preinit_array_start,
+                __preinit_array_end,
+                [](PreInitFuncType const func)
+                {
+                  func();
+                });
+}
+
+typedef void(*InitFuncType)(void);
+
+extern InitFuncType __init_array_start[];
+extern InitFuncType __init_array_end[];
+
+void init_array()
+{
+  std::for_each(__init_array_start,
+                __init_array_end,
+                [](InitFuncType const func)
+                {
+                  func();
+                });
+}
+
+typedef void(*FiniFuncType)(void);
+
+extern FiniFuncType __fini_array_start[];
+extern FiniFuncType __fini_array_end[];
+
+void fini_array()
+{
+  std::for_each(__fini_array_start,
+                __fini_array_end,
+                [](FiniFuncType const func)
+                {
+                  func();
+                });
 }
